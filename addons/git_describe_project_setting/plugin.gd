8222f06407ce2e6eb527418dd156185d7c92e3cd
zibetnu <zibetnu@pm.me>
2025-03-29 19:23:40 -0600

    Add editor plugin

---
+++ addons/git_describe_project_setting/plugin.gd
@@ -0,0 +1,30 @@
+@tool
+extends EditorPlugin
+
+const Debugger = preload("debugger.gd")
+const Exporter = preload("exporter.gd")
+const Utilities = preload("utilities.gd")
+
+var debugger := Debugger.new()
+var exporter := Exporter.new()
+
+
+func _enter_tree() -> void:
+	add_debugger_plugin(debugger)
+	add_export_plugin(exporter)
+	Utilities.print_errors()
+
+
+func _build() -> bool:
+	Utilities.update_version_setting()
+	return true
+
+
+func _disable_plugin() -> void:
+	remove_debugger_plugin(debugger)
+	remove_export_plugin(exporter)
+	Utilities.erase_version_setting()
+
+
+func _exit_tree() -> void:
+	_disable_plugin()
