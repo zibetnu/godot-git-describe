7dbb170e4d401028bda47397014d010d82ad1cdf
zibetnu <zibetnu@pm.me>
2025-04-05 18:36:20 -0600

    Add tag check to demo

--- demo/demo.gd
+++ demo/demo.gd
@@ -8,6 +8,7 @@ const Utilities = preload(
 @onready var platform_configured: CheckBox = %PlatformConfigured
 @onready var repository_found: CheckBox = %RepositoryFound
 @onready var git_found: CheckBox = %GitFound
+@onready var tag_found: CheckBox = %TagFound
 
 
 func _ready() -> void:
@@ -23,9 +24,15 @@ func _ready() -> void:
 			git_found,
 			Utilities.is_git_found()
 	)
-	git_describe.text = ProjectSettings.get_setting(
-			"application/config/version"
+	var is_tag_found: bool = Utilities.is_tag_found()
+	set_pressed_colorful(
+			tag_found,
+			is_tag_found
 	)
+	if is_tag_found:
+		git_describe.text = ProjectSettings.get_setting(
+				"application/config/version"
+		)
 
 
 func set_pressed_colorful(button: Button, value: bool) -> void:
