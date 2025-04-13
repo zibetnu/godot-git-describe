7bf60513fee495ce08a75f188a0b12fa7b34a703
zibetnu <zibetnu@pm.me>
2025-04-12 20:42:02 -0600

    Rename constants in settings

--- addons/git_describe/settings.gd
+++ addons/git_describe/settings.gd
@@ -1,28 +1,28 @@
 extends RefCounted
 
 const BASE = "addons/git_describe"
-const DESCRIBE_SETTING_NAME = BASE + "/describe_setting_path"
-const DEFAULT_DESCRIBE_SETTING_VALUE = "application/config/git_describe"
+const SETTING_PATH_SETTING = BASE + "/describe_setting_path"
+const DEFAULT_SETTING_PATH = "application/config/git_describe"
 
 
 static func init_settings() -> void:
-	if not ProjectSettings.has_setting(DESCRIBE_SETTING_NAME):
+	if not ProjectSettings.has_setting(SETTING_PATH_SETTING):
 		ProjectSettings.set_setting(
-				DESCRIBE_SETTING_NAME,
-				DEFAULT_DESCRIBE_SETTING_VALUE
+				SETTING_PATH_SETTING,
+				DEFAULT_SETTING_PATH
 		)
 
-	ProjectSettings.set_as_basic(DESCRIBE_SETTING_NAME, true)
+	ProjectSettings.set_as_basic(SETTING_PATH_SETTING, true)
 	ProjectSettings.set_initial_value(
-			DESCRIBE_SETTING_NAME,
-			DEFAULT_DESCRIBE_SETTING_VALUE
+			SETTING_PATH_SETTING,
+			DEFAULT_SETTING_PATH
 	)
 
 
 static func set_describe_setting(value: Variant) -> void:
 	var describe_setting: String = ProjectSettings.get_setting(
-			DESCRIBE_SETTING_NAME,
-			DEFAULT_DESCRIBE_SETTING_VALUE
+			SETTING_PATH_SETTING,
+			DEFAULT_SETTING_PATH
 	)
 	ProjectSettings.set_setting(describe_setting, value)
 	ProjectSettings.save()
