fd8f047f7d23f40e47e609136b77e8a97c23d67e
zibetnu <zibetnu@pm.me>
2025-04-12 20:35:52 -0600

    Revise describe label documentation

--- addons/git_describe/label/git_describe_label.gd
+++ addons/git_describe/label/git_describe_label.gd
@@ -1,7 +1,7 @@
 @icon("icon.svg")
 class_name GitDescribeLabel
 extends Label
-## A label for displaying Git describe text.
+## A label for automatically displaying Git describe strings.
 
 
 func _ready() -> void:
@@ -9,6 +9,7 @@ func _ready() -> void:
 
 
 ## Sets [member Label.text] to the describe string unless the string is empty.
+## Called on [signal Node.ready].
 func set_text_to_describe() -> void:
 	var describe_setting: String = ProjectSettings.get_setting(
 			"addons/git_describe/describe_setting_path",
