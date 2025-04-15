4f4d8c825cefb2d76e9ad7a1ae728582125be5df
zibetnu <zibetnu@pm.me>
2025-04-14 18:03:40 -0600

    Refactor setting initialization

--- addons/git_describe/settings.gd
+++ addons/git_describe/settings.gd
@@ -5,18 +5,20 @@ const SETTING_PATH_SETTING = BASE + "/describe_setting_path"
 const DEFAULT_SETTING_PATH = "application/config/git_describe"
 
 
-static func init_settings() -> void:
-	if not ProjectSettings.has_setting(SETTING_PATH_SETTING):
-		ProjectSettings.set_setting(
-				SETTING_PATH_SETTING,
-				DEFAULT_SETTING_PATH
-		)
+static func init_setting(
+		setting_name: String,
+		initial_value: Variant,
+		basic := true
+) -> void:
+	if not ProjectSettings.has_setting(setting_name):
+		ProjectSettings.set_setting(setting_name, initial_value)
 
-	ProjectSettings.set_as_basic(SETTING_PATH_SETTING, true)
-	ProjectSettings.set_initial_value(
-			SETTING_PATH_SETTING,
-			DEFAULT_SETTING_PATH
-	)
+	ProjectSettings.set_as_basic(setting_name, basic)
+	ProjectSettings.set_initial_value(setting_name, initial_value)
+
+
+static func init_settings() -> void:
+	init_setting(SETTING_PATH_SETTING, DEFAULT_SETTING_PATH)
 
 
 static func set_describe_setting(value: Variant) -> void:
