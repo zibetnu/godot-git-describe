4cef4d5f30d428d00d5e040988e1dba5bb5008f1
zibetnu <zibetnu@pm.me>
2025-06-24 16:37:55 -0600

    Add Git status check

--- git_describe_demo/demo.gd
+++ git_describe_demo/demo.gd
@@ -6,6 +6,7 @@ const Utilities = preload("res://addons/git_describe/utilities.gd")
 @onready var platform_configured: CheckBox = %PlatformConfigured
 @onready var repository_found: CheckBox = %RepositoryFound
 @onready var git_found: CheckBox = %GitFound
+@onready var git_status_ok: CheckBox = %GitStatusOK
 
 
 func _ready() -> void:
@@ -28,6 +29,10 @@ func run_checks() -> void:
 	set_pressed_colorful(
 			git_found,
 			Utilities.is_git_found()
+	)
+	set_pressed_colorful(
+			git_status_ok,
+			Utilities.execute("git status").exit_code == 0
 	)
 
 
